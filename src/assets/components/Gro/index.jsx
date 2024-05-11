196cac5b1d043d95aa37901a878422bb4ba397cd
abhishek <alabhishek17@gmail.com>
2024-05-12 00:16:05 +0530

    grocery ui is updated

--- src/assets/components/Gro/index.jsx
+++ src/assets/components/Gro/index.jsx
@@ -51,9 +51,10 @@ function Index() {
             setGroceryList(newCheckedList);
         }
     return (
-        <div>
-            <input type="text" placeholder="search" value={groceryInput} onChange={InputChange} />
-            <button onClick={addIteam}>Add</button>
+        <div style={{border:"2px solid black",width:"50%",textAlign:"center",padding:"40px",margin:"40px 50px 00px 400px",backgroundColor:"greenyellow"}}>
+            <h1 style={{marginBottom:"20px"}} >Grocery List</h1>
+            <input style={{padding:"20px"}} type="text" placeholder="search" value={groceryInput} onChange={InputChange} />
+            <button style={{padding:"20px", backgroundColor:"CaptionText",color:"white",marginLeft:"10px"}} onClick={addIteam}>Add</button>
 
             <section>
                 <ul>
--- src/assets/components/ListedIteam/lits.jsx
+++ src/assets/components/ListedIteam/lits.jsx
@@ -1,14 +1,14 @@
 function ListedItem({label="",deletFun,id,isChecked,checkedFun}){
     return(
         <div style={{listStyle:"none"}}>
-            <li>
+            <li style={{display:"flex",justifyContent:"space-between",marginLeft:"200px",marginTop:"40px",fontSize:"1.5rem",backgroundColor:"GrayText",width:"40%",padding:"20px"}}>
        <input type="checkbox" onClick={()=>checkedFun(id)}/>
        
        
        <p style={{textDecoration: isChecked ? "line-through":""}}>{label}</p>
        
 
-       <button onClick={()=>deletFun(id)}>delete</button>
+       <button style={{padding:"10px",backgroundColor:"black",color:"white"}} onClick={()=>deletFun(id)}>delete</button>
        </li>
         </div>
     )
